2ba4ad0970ae6a563c6bd902edef04a5392d47fe
LeviathanST <phamquochung170505@gmail.com>
2024-12-15 20:01:02 +0700

    feat(music): skip

--- src/command/music/mod.rs
+++ src/command/music/mod.rs
@@ -5,12 +5,16 @@ use crate::Context;
 mod join;
 mod leave;
 mod play;
+mod skip;
+mod volume;
 
 use join::*;
 use leave::*;
 use play::*;
+use skip::*;
+use volume::*;
 
-#[poise::command(slash_command, subcommands("join", "leave", "play"))]
+#[poise::command(slash_command, subcommands("join", "leave", "play", "skip"))]
 pub async fn music(_ctx: Context<'_>) -> Result<()> {
     Ok(())
 }
