3fbd97deca464bde771c9f738e01b3b132391cb3
akagiyuu <khanghuynh0245@gmail.com>
2024-12-26 12:49:30 +0700

    feat(meme): adapt commands

--- src/command/meme/get.rs
+++ src/command/meme/get.rs
@@ -5,13 +5,11 @@ use crate::{command::Meme, Context, CONFIG};
 
 use super::fuzzy;
 
-async fn autocomplete_name<'a>(ctx: Context<'a>, partial: &'a str) -> Vec<String> {
-    let mut meme_finder = ctx.data().meme_finder.lock().await;
-
-    fuzzy(&mut meme_finder, partial)
-        .await
-        .map(|(_, name)| name.clone())
-        .collect()
+async fn autocomplete_name<'a>(
+    ctx: Context<'a>,
+    partial: &'a str,
+) -> impl Iterator<Item = String> + 'a {
+    fuzzy(&ctx.data().memes, partial).await
 }
 
 #[poise::command(slash_command)]
@@ -19,12 +17,12 @@ pub async fn get(
     ctx: Context<'_>,
     #[autocomplete = "autocomplete_name"] name: String,
 ) -> Result<()> {
-    let mut meme_finder = ctx.data().meme_finder.lock().await;
-    let id = fuzzy(&mut meme_finder, &name)
-        .await
-        .next()
-        .map(|(id, _)| *id)
-        .ok_or(anyhow!("No matching meme"))?;
+    let id = *ctx
+        .data()
+        .memes
+        .get(&name)
+        .ok_or(anyhow!("Meme does not exist"))?
+        .value();
 
     let meme_raw = ChannelId::new(CONFIG.data_channel_id)
         .message(ctx.http(), id)
