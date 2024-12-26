b989631c52121b8fb2be53649324d14746eacd75
akagiyuu <khanghuynh0245@gmail.com>
2024-12-26 12:48:25 +0700

    fix: remove not existed module

--- src/command/music/mod.rs
+++ src/command/music/mod.rs
@@ -6,13 +6,11 @@ mod join;
 mod leave;
 mod play;
 mod skip;
-mod volume;
 
 use join::*;
 use leave::*;
 use play::*;
 use skip::*;
-use volume::*;
 
 #[poise::command(slash_command, subcommands("join", "leave", "play", "skip"))]
 pub async fn music(_ctx: Context<'_>) -> Result<()> {
